b49add81f0ce8684d719c54b19bc92790e1c3a79
Sadid369 <sadid.jones@gmail.com>
2022-03-25 19:56:57 +0600

    ....

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors
 
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:food_delivery2/utils/colors.dart';
 import 'package:food_delivery2/widgets/big_text.dart';
@@ -15,6 +17,24 @@ class FoodPageBody extends StatefulWidget {
 
 class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
+  var _currentPageValue = 0.0;
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currentPageValue = pageController.page!;
+        log("_FoodPageBodyState: Current page value is:  $_currentPageValue");
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -29,6 +49,10 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
+    Matrix4 matrix = new Matrix4.identity();
+    if(index == _currentPageValue.floor()){
+      var currScale = 1-(_currentPageValue-index)*
+    }
     return Stack(
       children: [
         Container(
