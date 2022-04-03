c4a7160cd3e1d246717402c5450255d5468c1d41
Sadid369 <sadid.jones@gmail.com>
2022-04-03 19:22:09 +0600

    @ 03/04/2022 @ 5:35:32

--- lib/pages/food/recommended_food_detail.dart
+++ lib/pages/food/recommended_food_detail.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery2/utils/colors.dart';
 import 'package:food_delivery2/utils/dimensions.dart';
+import 'package:food_delivery2/widgets/app_icon.dart';
 import 'package:food_delivery2/widgets/big_text.dart';
+import 'package:food_delivery2/widgets/expandale_text_widget.dart';
 
 class RecommendedFoodDetail extends StatelessWidget {
   const RecommendedFoodDetail({Key? key}) : super(key: key);
@@ -12,10 +14,21 @@ class RecommendedFoodDetail extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            toolbarHeight: 70,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(
+                  icon: Icons.clear,
+                ),
+                AppIcon(
+                  icon: Icons.shopping_cart_outlined,
+                ),
+              ],
+            ),
             bottom: PreferredSize(
               preferredSize: Size.fromHeight(20),
               child: Container(
-                color: Colors.white,
                 child: Center(
                     child: BigText(
                   text: "Chinese Side",
@@ -25,6 +38,17 @@ class RecommendedFoodDetail extends StatelessWidget {
                 padding: EdgeInsets.only(
                   top: 5,
                   bottom: 10,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(
+                      Dimensions.radius20,
+                    ),
+                    topRight: Radius.circular(
+                      Dimensions.radius20,
+                    ),
+                  ),
                 ),
               ),
             ),
@@ -40,8 +64,12 @@ class RecommendedFoodDetail extends StatelessWidget {
             ),
           ),
           SliverToBoxAdapter(
-            child: Text(
-                '''If you’re planning an Asian-themed dinner, these incredible Chinese side dishes will make it a feast to remember.
+              child: Column(
+            children: [
+              Container(
+                child: ExpandableTextWidget(
+                    text:
+                        '''If you’re planning an Asian-themed dinner, these incredible Chinese side dishes will make it a feast to remember.
                   
                   From flavorful veggies to filling rice and noodle dishes, these recipes are sure to complement any main course!If you’re planning an Asian-themed dinner, these incredible Chinese side dishes will make it a feast to remember.
                   
@@ -58,7 +86,106 @@ class RecommendedFoodDetail extends StatelessWidget {
                   From flavorful veggies to filling rice and noodle dishes, these recipes are sure to complement any main course!If you’re planning an Asian-themed dinner, these incredible Chinese side dishes will make it a feast to remember.
                   
                   From flavorful veggies to filling rice and noodle dishes, these recipes are sure to complement any main course!'''),
-          )
+                margin: EdgeInsets.only(
+                  left: Dimensions.width20,
+                  right: Dimensions.width20,
+                ),
+              ),
+            ],
+          ))
+        ],
+      ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              left: Dimensions.width20 * 2.5,
+              right: Dimensions.width20 * 2.5,
+              top: Dimensions.height10,
+              bottom: Dimensions.height10,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(
+                  icon: Icons.remove,
+                  backgroundColor: AppColors.mainColor,
+                  iconColor: Colors.white,
+                  iconSize: Dimensions.iconeSize24,
+                ),
+                BigText(
+                  text: "\$12.88" + " X " + " 0 ",
+                  color: AppColors.mainBlackColor,
+                  size: Dimensions.font26,
+                ),
+                AppIcon(
+                  icon: Icons.add,
+                  backgroundColor: AppColors.mainColor,
+                  iconColor: Colors.white,
+                  iconSize: Dimensions.iconeSize24,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: Dimensions.bottomHeightBar,
+            padding: EdgeInsets.only(
+                top: Dimensions.height30,
+                bottom: Dimensions.height30,
+                left: Dimensions.width20,
+                right: Dimensions.width20),
+            decoration: BoxDecoration(
+              color: AppColors.buttonBackgroundColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(
+                  Dimensions.radius20 * 2,
+                ),
+                topRight: Radius.circular(
+                  Dimensions.radius20 * 2,
+                ),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                    padding: EdgeInsets.only(
+                      top: Dimensions.height20,
+                      bottom: Dimensions.height20,
+                      left: Dimensions.width20,
+                      right: Dimensions.width20,
+                    ),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(
+                          Dimensions.radius20,
+                        ),
+                        color: Colors.white),
+                    child: Icon(
+                      Icons.favorite,
+                      color: AppColors.mainColor,
+                    )),
+                Container(
+                  padding: EdgeInsets.only(
+                    top: Dimensions.height20,
+                    bottom: Dimensions.height20,
+                    left: Dimensions.width20,
+                    right: Dimensions.width20,
+                  ),
+                  child: BigText(
+                    text: "\$10 | Add to cart",
+                    color: Colors.white,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      Dimensions.radius20,
+                    ),
+                    color: AppColors.mainColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
